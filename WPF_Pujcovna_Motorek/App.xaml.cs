fea8db1f3f8455a7a574b65e974e315ccc57ee0f
Iwcas <129670135+Iwcas@users.noreply.github.com>
2025-04-05 01:47:22 +0200

    Add project files.

---
+++ WPF_Pujcovna_Motorek/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Pujcovna_Motorek
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
