8ca192380c7c7347a92a1a4496cb99c914a436c7
Iwcas <129670135+Iwcas@users.noreply.github.com>
2025-04-05 03:28:11 +0200

    Created SQLRepository.cs and added function NactiVypujcky

---
+++ WPF_Pujcovna_Motorek/Class/SQL_Repository/SqlRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace WPF_Pujcovna_Motorek.Class.SQL_Repository
+{
+    class SqlRepository
+    {
+        public string Connection_String { get; set; }
+
+        public SqlRepository(string connection_String)
+        {
+            Connection_String = connection_String;
+        }
+
+        public List<Vypujcka> NactiVypujcky(string sloupecTrideni, bool sestupne, int hledani)
+        {
+            List<Vypujcka> vypujcka = new List<Vypujcka>();
+            using (SqlConnection conn = new SqlConnection())
+            {
+                using(SqlCommand cmd = new SqlCommand("", conn))
+                {
+                    string text_hledani = hledani == 0 ? "" : $"where Vypujcky.IdZakaznik = {hledani} ";
+                    string jak_Tridit = !sestupne ? "asc" : "desc";
+                    cmd.CommandText = $"" +
+                        $"select Vypujcky.Id as VId, Jmeno, Prijmeni, Motorka.Id as MId, Zakaznik.Id as ZId, Motorka.Nazev as MNazev," +
+                        $" Motorka.model as MModel, Motorka.Najezd as MNajezd, Motorka.Aktualni_Nadrz as MAkt_nadrz, " +
+                        $"Motorka.RokVyroby as MRok, Motorka.Barva as MBarva, Vypujcky.Pujceno as VPujceno, " +
+                        $"Vypujcky.Vraceno as VVraceno" +
+                        $"from Vypujcky " +
+                        $"inner join Zakaznik on (Vypujcky.Id_Zakznik = Zakaznik.Id) " +
+                        $"inner join Motorka on (Vypujcky.Id_Motorka = Motorka.Id) " +
+                        $"{text_hledani}" +
+                        $"order by {sloupecTrideni} {jak_Tridit};";
+                    conn.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            vypujcka.Add(new Vypujcka(int.Parse(dr["VId"].ToString()),
+                                int.Parse(dr["ZId"].ToString()),
+                                int.Parse(dr["MId"].ToString()),
+                                dr.GetDateTime("VPujceno"),
+                                !dr.IsDBNull("VVraceno") ? dr.GetDateTime("VVraceno") : null,
+                                dr["Jmeno"].ToString().Trim(),
+                                dr["Prijmeni"].ToString().Trim(),
+                                dr["MNazev"].ToString().Trim()));
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            return vypujcka;
+        }
+    }
+}
---
+++ WPF_Pujcovna_Motorek/SQLQuery/SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿select Vypujcky.Id as VId, Jmeno, Prijmeni, Motorka.Id as MId, Zakaznik.Id as ZId, Motorka.Nazev as MNazev,
+                        Motorka.model as MModel, Motorka.Najezd as MNajezd, Motorka.Aktualni_Nadrz as MAkt_nadrz,
+                        Motorka.RokVyroby as MRok, Motorka.Barva as MBarva, Vypujcky.Pujceno as VPujceno,
+                        Vypujcky.Vraceno as VVraceno
+                        from Vypujcky
+                        inner join Zakaznik on (Vypujcky.Id_Zakaznik = Zakaznik.Id)
+                        inner join Motorka on (Vypujcky.Id_Motorka = Motorka.Id)
